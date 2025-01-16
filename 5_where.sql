43dda848d57369e49e59f8e766f405934a57f019
Ekrem YILDIRIM <84474522+ekremyldrm@users.noreply.github.com>
2025-01-16 21:27:19 +0300

    Add files via upload

---
+++ 5_where.sql
@@ -0,0 +1,74 @@
+﻿select * from Products
+where UnitsInStock > 20 and UnitsInStock < 40
+
+select * from Products
+where UnitsInStock >= 20 and UnitsInStock <= 40
+
+--between operatörü.. belirli aralığa göre veri getirmek için kullanılır
+select * from Products
+where UnitsInStock between 20 and 40
+
+select GETDATE() -- tarih verisini döndürür,
+select YEAR(getdate()) -- yıl bilgisini döner
+select MONTH(getdate()) -- ay bilgisini döndürür
+select DAY(getdate())
+
+select GETDATE() AS TARIH,YEAR(GETDATE()) AS YIL,MONTH(GETDATE()) AS AY
+,DAY(GETDATE()) AS GUN
+
+select YEAR(OrderDate),MONTH(OrderDate),DAY(OrderDate), * from Orders
+
+SELECT * FROM Orders
+WHERE YEAR(OrderDate)  = 1998
+
+SELECT * FROM Orders
+WHERE YEAR(OrderDate)  = 1998 AND MONTH(OrderDate) BETWEEN 1 AND 3
+
+
+
+-- like => benzer . veritabanında koloında arama yapmak için kullanılır
+select * from Products
+where ProductName like '%ku%' -- içerisinde geçen
+
+select * from Products
+where ProductName like 'ch%' -- ile başlayan
+
+select * from Products
+where ProductName like '%ku' -- ile biten
+
+-- not like 
+select * from Products
+where ProductName not like '%ku%' -- içerisinde geçmeyen
+
+select * from Products
+where ProductName not like 'ch%' -- ile başlamayan
+
+select * from Products
+where ProductName not like '%ku' -- ile bitmeyen
+
+-- is null
+select * from Suppliers
+where Region is null
+
+-- is not null
+select * from Suppliers
+where Region is not null
+
+
+
+
+
+Select * from Orders where 1=2
+
+SELECT UPPER('EkReM'),LOWER('GökBerK')
+
+select 'ekrem' where 1<2
+select 'ekrem' where 'a'='A'
+
+-- Description alanı sql kodu olduğu için yazılışta sql ifadesine benzer.. buyüzden isimlendirme yaparken sql kodlarını obje isimlrimize vermemeliyiz... eğer bu alanisimi normal isim gibi görmek isterseniz [] kullanabilirsiniz
+select CategoryName, [Description] from Categories
+
+-- obje isimlerinde boşluk kullanılmaz...
+select * from [Order Details]
+
+select  CategoryName as [Kategori Adı], Description as Aciklama  from Categories
---
+++ 6_aggregateFunc&GroupBy.sql
@@ -0,0 +1,99 @@
+﻿use Northwind
+
+--aggregate functions
+select max(UnitPrice) from Products
+
+select min(UnitPrice) from Products
+
+select min(UnitPrice) from Products
+where CategoryID = 1
+
+select avg(UnitPrice) from Products
+
+
+select sum(UnitPrice) from Products
+
+-- tablodaki satır sayısını döndürür...
+select count(*) from Products
+
+-- count içerisinde * kullanılırsa satırların tamamını sayar
+-- count içerisinde * yerine kolon ismi yazılırsa satırın tamamı yerine null olmayan satırları sayar
+select COUNT(*),count(s.SupplierID),Count(s.Fax) from Suppliers s
+
+-- YUKARIDAKİ AGGREGATE FUNCTİONLAR GERİYE TEK SATIR TEK SÜTUN DÖNERLER...
+
+--GROUP BY => TABLODAKİ ALANLARI BELİRLİ BİR ALANA GÖRE GRUPLAMAYA YARAR... 
+-- group by kullanımında group by uygulanan satır select ifadesinde seçilmek zorundadır. 
+
+SELECT  CategoryID FROM Products
+GROUP BY CategoryID
+
+select CategoryID from Products
+group by SupplierID
+
+select ProductID from [Order Details]
+group by ProductID
+
+-- CategoryId'ye göre ürün sayısı
+SELECT CategoryID,cOUNT(*) FROM Products
+GROUP BY CategoryID
+
+
+select ProductID,Count(*) from [Order Details]
+group by ProductID
+
+
+select ProductID,Sum(od.Quantity) as Adet from [Order Details] as od
+group by ProductID
+
+
+select ProductID,Count(*) as SiparisSayi,Sum(od.Quantity) as SiparisMiktari from [Order Details] as od
+group by ProductID
+
+-- group by 'da where kullanılmaz...
+-- having ifadesi group by querylerinde kriter belirlemek için kullanılır...
+
+--ProductId'ye göre ProductId'si 1 olanları grubla 
+-- kaynaktan id'si 1 olanları al ve grupla
+select ProductID,Count(*) as SiparisSayi,Sum(od.Quantity) as SiparisMiktari from [Order Details] as od
+where ProductId = 1
+group by ProductID
+
+-- ProductID'ye göre grupla, 1 olanları getir
+-- kaynaktan tamanını al ve grupla
+select ProductID,Count(*) as SiparisSayi,Sum(od.Quantity) as SiparisMiktari from [Order Details] as od
+group by ProductID
+having ProductId = 1 -- gruplu verilerde kriter belirler...
+
+
+select ShipCountry from Orders
+WHERE ShipCountry = 'Spain'
+group by ShipCountry
+Having ShipCountry  = 'UK'
+
+select ShipCountry from Orders
+group by ShipCountry
+Having ShipCountry  = 'UK'
+
+select ShipCountry,COunt(*) from Orders
+group by ShipCountry
+Having ShipCountry  = 'UK'
+
+--nakliyecilerin taşıma yaptıkları ülkeler
+select o.ShipVia,o.ShipCountry,Count(*)  from Orders o
+group by o.ShipVia,o.ShipCountry 
+
+select o.ShipVia,o.ShipCountry,o.ShipCity ,Count(*)  from Orders o
+group by o.ShipVia,o.ShipCountry,o.ShipCity
+
+select o.ShipVia,o.ShipCountry,o.ShipCity ,Count(*)  from Orders o
+group by o.ShipVia,o.ShipCountry,o.ShipCity
+having o.ShipCountry ='UK' AND O.ShipCity= 'London'
+
+-- subquery,joins,insert,update,delete
+
+
+
+
+
+
