d7c2fe56e83c504ba666bd5701333bf0d5e8a0e7
Ekrem YILDIRIM <84474522+ekremyldrm@users.noreply.github.com>
2025-01-23 21:23:43 +0300

    Add files via upload

---
+++ 10_insert.sql
@@ -0,0 +1,73 @@
+﻿
+insert Ogrenciler
+(Adi)
+values
+('Emine')
+
+
+insert Ogrenciler
+(Adi)
+values
+('Gökberk')
+
+insert Ogrenciler
+(Adi)
+values
+('Veli')
+
+
+-- çoklu insert
+insert Ogrenciler
+(Adi)
+values
+('Ern'),
+('Mahmut')
+
+-- Identity alanlara insert işlemi yapılamaz
+--insert Ogrenciler
+--(Id,Adi)
+--values
+--(10,'Şimal')
+
+
+
+insert Shippers
+(CompanyName,Phone)
+values
+('Yıldırım Jet Kargo','191919')
+
+
+select * from Ogrenciler
+select * from Shippers
+
+
+-- tablpdaki verileri günellemek için UPDATE komutu kullanılır...
+
+-- UPDATE VE DELETE KOMUTLARI BÜTÜN TABLOYU ETKİLER. WHERE KRİTERİ OLMADAN KULLANMAYINIZ...
+update Ogrenciler
+	set Adi = 'Eren'
+where Adi = 'Ern'
+
+update Ogrenciler
+	set Adi = 'EMİNE'
+WHERE Adi = 'Emine'
+
+-- update ve delete ifadelerini pklar üzerinden yazınız
+update Shippers
+	set CompanyName = 'YJK (Yıldırım Jet Kargo)',Phone= '5413510019'
+where ShipperID = 4
+
+select * from Shippers
+
+select * from Ogrenciler
+
+-- delete tablodaki verileri silmek için kullanılır...
+delete Ogrenciler
+where Id = 2
+
+delete Ogrenciler
+where Id = 3
+
+delete Ogrenciler
+where Id = 1443
+
---
+++ 9_joins.sql
@@ -0,0 +1,48 @@
+﻿select * from Categories c
+join Products p on c.CategoryID = p.CategoryID
+
+
+
+-- inner join ve join aynı şeydir
+--left join ve left outer join aynı şeydir
+--right join ve right outer join...
+--full join ve full outer join...
+
+select * from Categories c 
+left outer join Products p on c.CategoryID = p.CategoryID
+
+
+-- ürünü olmayan kategoriler
+
+-- sub query
+select * from Categories
+where CategoryID not in (select CategoryID from Products where CategoryID is not null)
+
+--outer join
+select * from Categories c 
+left join Products p on c.CategoryID = p.CategoryID
+where p.CategoryID is null
+
+
+
+
+--right join 
+select* from Categories c
+right join Products p
+on c.CategoryID = p.CategoryID
+
+-- yukarıdaki sonucu left yazarakta elde edebiliriz
+select * from Products p
+left join Categories c on c.CategoryID = p.CategoryID
+
+-- full join
+select *from Categories c
+--full outer join Products p on c.CategoryID = p.CategoryID
+full join Products p on c.CategoryID = p.CategoryID
+
+
+-- corss join
+-- cross join bütün kayıtları eşleştirir
+select * from Ogrenciler o
+cross join Dersler d
+
